18718f895eca5b529b580c9df10036bf7a3a58d1
League Student <league@iMac-10.attlocal.net>
2020-01-02 19:48:17 -0800

    Create Binary.java

---
+++ src/_07_binary_converter/Binary.java
@@ -0,0 +1,12 @@
+package _07_binary_converter;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Binary {
+public static void main(String[] args) {
+JFrame q = new JFrame();
+JPanel w = new JPanel();
+
+}
+}
