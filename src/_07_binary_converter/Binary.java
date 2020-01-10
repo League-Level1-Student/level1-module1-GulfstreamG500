2a37f9599f8e7ef1042f6b632ab213dde4de6e0f
League Student <league@iMac-17.attlocal.net>
2020-01-09 19:48:26 -0800

    dsf
    dsfg

--- src/_07_binary_converter/Binary.java
+++ src/_07_binary_converter/Binary.java
@@ -1,12 +1,83 @@
 package _07_binary_converter;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
-public class Binary {
+public class Binary implements MouseListener {
+	JTextField text = new JTextField(20);
 public static void main(String[] args) {
-JFrame q = new JFrame();
-JPanel w = new JPanel();
+	Binary b = new Binary();
+b.important();
+}
+public void important() {
+	
 
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+frame.setVisible(true);
+panel.setVisible(true);
+
+JLabel label = new JLabel();
+JButton button = new JButton();
+button.addMouseListener(this);
+frame.add(panel);
+panel.add(text);
+panel.add(button);
+panel.add(label);
+frame.pack();
+
+}
+
+String convert(String input) {
+    if(input.length() != 8){
+        JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+        return "-";
+    }
+    String binary = "[0-1]+";//must contain numbers in the given range
+    if (!input.matches(binary)) {
+        JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+        return "-";   
+    }
+    try {
+        int asciiValue = Integer.parseInt(input, 2);
+        char theLetter = (char) asciiValue;
+        return "" + theLetter;
+    } catch (Exception e) {
+        JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+        return "-";
+    }
+}
+
+@Override
+public void mouseClicked(MouseEvent e) {
+	// TODO Auto-generated method stub
+
+}
+@Override
+public void mousePressed(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseReleased(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseEntered(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+@Override
+public void mouseExited(MouseEvent e) {
+	// TODO Auto-generated method stub
+	
 }
 }
